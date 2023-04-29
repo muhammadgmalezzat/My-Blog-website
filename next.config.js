51e0e44108e62701f4ff9d3a0e422f246c6463e4
muhammad gmal ezzat <59686905+muhammadgmalezzat@users.noreply.github.com>
2023-04-29 17:35:02 +0300

    deployment finished

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
+
+export default (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
             env: {
                 mongodb_username: 'gemy',
                 mongodb_password: 'gemy200',
@@ -6,4 +10,14 @@ module.exports = {
                 mongodb_database: "my-site-development"
             },
         };
+  }
 
+  return {
+        env: {
+            mongodb_username: 'gemy',
+            mongodb_password: 'gemy200',
+            mongodb_clustername: 'blog-database',
+            mongodb_database: "my-site-production"
+        },
+    };
+};
