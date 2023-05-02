b8f9c581ddbfb4a3298aa2cad2eda6bdea7db7e6
muhammad gmal ezzat <59686905+muhammadgmalezzat@users.noreply.github.com>
2023-05-02 11:29:44 +0300

    social media links add

--- components/homePage/hero.js
+++ components/homePage/hero.js
@@ -1,5 +1,14 @@
 import styles from './hero.module.css'
 import Image from 'next/image';
+import {BsLinkedin ,BsFacebook ,BsGithub, BsBehance,BsInstagram }from 'react-icons/bs'
+import {
+    SlSocialLinkedin,
+    SlSocialFacebook,
+    SlSocialInstagram,
+    SlSocialGithub,
+    SlSocialBehance
+} from 'react-icons/sl'
+
 const hero = () => {
     return (
         <section className={styles.hero}>
@@ -15,6 +24,41 @@ const hero = () => {
             <p>
                 I blog about web development - especially frontend frameworks like React.
             </p>
+            <div className={styles.sociallinks}>
+                <ul >
+                    <li>
+                        <a href='https://github.com/muhammadgmalezzat' target="_blank">
+                        <span>
+                            <BsGithub />
+                        </span>
+                    </a>
+                    </li>
+                    <li><a href='https://www.facebook.com/mohamed.gmal.98871' target="_blank">
+                        <span>
+                            <BsFacebook />
+                        </span>
+                    </a></li>
+
+                    <li><a href='https://www.linkedin.com/in/muhammad-gmal-61330119b/' target="_blank">
+                        <span>
+                            <BsLinkedin />
+                        </span>
+                    </a></li>
+
+                    <li><a href='https://www.instagram.com/7958_muhammad/?fbclid=IwAR2eHRWktRWm-FDqcPWCFYOcm7FrbFBtr0X2mBRjMpjZWfCMcXrarMwtSg4' target="_blank">
+                        <span>
+                            <BsInstagram />
+                        </span>
+                    </a></li>
+
+                    <li><a href='https://www.behance.net/muhammad12' target="_blank">
+                        <span>
+                            <BsBehance />
+                        </span>
+                    </a></li>
+                </ul>
+            </div>
+            
         </section>
     )
 }
